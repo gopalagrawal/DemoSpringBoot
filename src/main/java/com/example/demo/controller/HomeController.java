5da5b3bd5969ee5ef3a1826bbbbae47df647f366
ga <ga.willbe@gmail.com>
2019-06-12 11:13:55 +0530

    Automapping of params to our controller & using MV to set data/view.

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,12 +1,11 @@
 package com.example.demo.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -14,17 +13,29 @@ public class HomeController {
 	@Value("${message.default.welcome}")
 	private String welcomemsg;
 	
-	@RequestMapping("/home")
-	public String home(HttpServletRequest req, HttpServletResponse res) {
-		String name = req.getParameter("name");
-		
+	
+	//public String home(HttpServletRequest req, HttpServletResponse res) {
+	//
+	// Let's get rid of req, res objects. Automapped by springboot from req parameter. 
+	// If req.param is different than name used here (or having multiple params),  
+	//		map with ... ( @RequestParam("name") String myName, ...... )
+	// else if single param with same name: 
+	//		map with ... ( String name ..... 
+	//
+	@RequestMapping("home") //even "/home" will work. 
+	public ModelAndView  home(String name, HttpSession s) {		
 		System.out.println(welcomemsg + " " + name);
 		
-		//now we can send data to user either in req object, session, cookies etc. 
-		HttpSession s = req.getSession();
-		s.setAttribute("welcomemsg", welcomemsg);
-		s.setAttribute("name", name);
+		//s.setAttribute("welcomemsg", welcomemsg);
+		//s.setAttribute("name", name);
+		//return "home";  
 		
-		return "home";  
+		//Let's use ModelAndView Instead of session. 
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("welcomemsg", welcomemsg);
+		mv.addObject("name", name);
+		mv.setViewName("home");
+		return mv;
+		
 	}
 }
